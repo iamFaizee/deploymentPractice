75291dda9dcb3802da6532faa501dff8209b5f5d
iamFaizee <khanfaizan1123@gmail.com>
2023-06-25 18:39:25 +0530

    Basic express deployment

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+require('dotenv').config()
+
+
+const app = express()
+
+
+app.get('/', (req,res) => {
+    res.send("Welcome to Homepage")
+})
+
+
+app.get('/product', (req,res) => {
+    res.json({success: "this is product"})
+})
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`listening on port ${process.env.PORT}`)
+})
